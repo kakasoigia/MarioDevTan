396c27bd422744cd21bc9a686efe7991a273ec7b
kakasoigia <kakasoigia@gmail.com>
2020-12-09 17:06:44 +0700

    Xin trả lại mọi thứ nguyên vẹn

---
+++ Super_Mario_Bros3/BackgroundStage.cpp
@@ -0,0 +1,156 @@
+#include "BackgroundStage.h"
+
+
+
+CBackGroundStage::CBackGroundStage(int ctype)
+{
+	type = ctype;
+	if (type != BACKGROUND_STAGE_TYPE_BLACK)
+		isAppear = false;
+	SetState(BACKGROUND_STAGE_STATE_IDLE);
+}
+
+
+
+
+void CBackGroundStage::GetBoundingBox(float &l, float &t, float &r, float &b)
+{
+	l = t = r = b = 0;
+}
+
+void CBackGroundStage::Update(DWORD dt, vector<LPGAMEOBJECT> *coObjects)
+{
+	CGameObject::Update(dt);
+
+	vector<LPCOLLISIONEVENT> coEvents;
+	vector<LPCOLLISIONEVENT> coEventsResult;
+
+	coEvents.clear();
+
+	StartShowing();
+
+	if (GetTickCount() - time_showing >= 4300)
+	{
+		if (type == BACKGROUND_STAGE_TYPE_COLOR)
+		{
+			isAppear = true;
+			StartDown();
+		}
+	}
+
+	if (GetTickCount() - time_showing >= 7000)
+	{
+		if (type == BACKGROUND_STAGE_TYPE_FINAL)
+		{
+			isAppear = true;
+		}
+	}
+
+
+	if (type == BACKGROUND_STAGE_TYPE_COLOR && isAppear)
+	{
+		if (GetTickCount() - time_down >= 500)
+		{
+			SetState(BACKGROUND_STAGE_STATE_IDLE);
+		}
+		else
+		{
+			SetState(BACKGROUND_STAGE_STATE_DOWN);
+		}
+		if (GetTickCount() - time_down >= 700)
+		{
+			SetState(BACKGROUND_STAGE_STATE_SHAKE);
+		}
+		if (GetTickCount() - time_down >= 1400)
+		{
+			SetState(BACKGROUND_STAGE_STATE_IDLE);
+		}
+
+	}
+
+	if (state == BACKGROUND_STAGE_STATE_SHAKE)
+	{
+		StartShake();
+		if (GetTickCount() - time_shake >= 25)
+		{
+			this->vy = 0.2f *shakeDirection;
+			shakeDirection = -shakeDirection;
+			time_shake = 0;
+		}
+	}
+
+
+	// No collision occured, proceed normally
+	if (coEvents.size() == 0)
+	{
+		x += dx;
+		y += dy;
+	}
+	else
+	{
+
+		float min_tx, min_ty, nx = 0, ny;
+		float rdx = 0;
+		float rdy = 0;
+
+		// TODO: This is a very ugly designed function!!!!
+		FilterCollision(coEvents, coEventsResult, min_tx, min_ty, nx, ny, rdx, rdy);
+
+		// block 
+		//x += min_tx * dx + nx * 0.4f;		// nx*0.4f : need to push out a bit to avoid overlapping next frame
+		//y += min_ty * dy +  ny * 0.4f;
+
+		//if (nx != 0) vx = 0;
+		//if (ny != 0) vy = 0;
+
+		// Collision logic with the others Goombas
+		for (UINT i = 0; i < coEventsResult.size(); i++)
+		{
+			LPCOLLISIONEVENT e = coEventsResult[i];
+		}
+	}
+
+
+	// clean up collision events
+	for (UINT i = 0; i < coEvents.size(); i++) delete coEvents[i];
+
+}
+
+void CBackGroundStage::Render()
+{
+	int ani = -1;
+	if (isAppear)
+	{
+		switch (type)
+		{
+		case BACKGROUND_STAGE_TYPE_BLACK:
+			ani = BACKGROUND_STAGE_TYPE_BLACK_ANI;
+			break;
+		case BACKGROUND_STAGE_TYPE_COLOR:
+			ani = BACKGROUND_STAGE_TYPE_COLOR_ANI;
+			break;
+		case BACKGROUND_STAGE_TYPE_FINAL:
+			ani = BACKGROUND_STAGE_TYPE_FINAL_ANI;
+			break;
+		}
+	}
+	else
+		return;
+	animation_set->at(ani)->Render(x, y);
+
+	//RenderBoundingBox();
+}
+
+void CBackGroundStage::SetState(int state)
+{
+	CGameObject::SetState(state);
+	switch (state)
+	{
+	case  BACKGROUND_STAGE_STATE_IDLE:
+		vx = vy = 0;
+		break;
+	case  BACKGROUND_STAGE_STATE_DOWN:
+		vy = 0.1f;
+		break;
+	}
+}
---
+++ Super_Mario_Bros3/BackgroundStage.h
@@ -0,0 +1,69 @@
+#pragma once
+#include "GameObject.h"
+#include "algorithm"
+#include "Mario.h"
+#include "PlayScence.h"
+
+
+#define BACKGROUND_STAGE_STATE_IDLE		0
+#define BACKGROUND_STAGE_STATE_DOWN		100
+#define BACKGROUND_STAGE_STATE_SHAKE	200
+
+
+#define BACKGROUND_STAGE_TYPE_BLACK_ANI		0	
+#define BACKGROUND_STAGE_TYPE_COLOR_ANI		1
+#define BACKGROUND_STAGE_TYPE_FINAL_ANI		2
+
+
+#define BACKGROUND_STAGE_TYPE_BLACK		111
+#define BACKGROUND_STAGE_TYPE_COLOR		222
+#define BACKGROUND_STAGE_TYPE_FINAL		333
+
+class CBackGroundStage : public CGameObject
+{
+	DWORD time_showing = 0;
+	DWORD time_down = 0;
+	DWORD time_shake = 0;
+
+	int type;
+	int shakeDirection = 1;
+	bool isAppear = true;
+
+public:
+	virtual void GetBoundingBox(float &left, float &top, float &right, float &bottom);
+	virtual void Update(DWORD dt, vector<LPGAMEOBJECT> *coObjects);
+	virtual void Render();
+	CBackGroundStage(int ctype);
+	virtual void SetState(int state);
+	int GetType()
+	{
+		return type;
+	}
+	void SetType(int typeInt)
+	{
+		type = typeInt;
+	}
+	void StartShowing()
+	{
+		if (time_showing == 0)
+			time_showing = GetTickCount();
+	}
+	void StartDown()
+	{
+		if (time_down == 0)
+			time_down = GetTickCount();
+	}
+	void StartShake()
+	{
+		if (time_shake == 0)
+			time_shake = GetTickCount();
+	}
+	bool GetIsAppear()
+	{
+		return isAppear;
+	}
+	void SetIsAppear(bool IsAppearBool)
+	{
+		this->isAppear = IsAppearBool;
+	}
+};
